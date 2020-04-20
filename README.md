56e76521dbf3bd44c0d1cb998a7b4789681aa585
Russell Thomason <rthomason@gmail.com>
2020-04-19 17:27:16 -0700

    documentation, fix last query

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # crowdstreet-sql
+
+This is a bash script that solves the SQL portion of the Crowdstreet assignment.
+
+## Setup
+
+There are 3 postgres scripts included in this project
+ - `create-db.psql`: creates a postgres database and necessary tables
+ - `insert-db.psql`: inserts the initial data
+ - `query-db.psql` : performs the required queries
+ 
+ There is a run script which will run the above postgres scripts.
+ Be sure to update the script for the correct host / port / user.
+ 
--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# use this script to create the database and tables, and then insert data
-# be sure to fill in the correct host, port, and user
+# Use this script to create the database and tables, insert data, and run queries.
+#
+# Be sure to fill in the correct host, port, and user.
 
+echo "- Creating database and tables -"
 psql -h localhost -p 5432 -U russ -f create-db.psql
+
+echo "- Inserting initial data into tables -"
 psql -h localhost -p 5432 -U russ -d crowdstreet_demo -f insert-db.psql
 
+echo "- Running queries -"
+psql -h localhost -p 5432 -U russ -d crowdstreet_demo -f query-db.psql
